d25426ed62615403d8b6e0525365ac8dfc74bb42
Sighhh Kolinsky <sighhh0704@gmail.com>
2024-07-26 19:22:23 +0800

    Structural changes

--- Number-Bitch/Number-Bitch/resource.h
+++ Number-Bitch/Number-Bitch/resource.h
@@ -2,8 +2,7 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 Number-Bitch.rc 使用
 //
-#define IDI_CapsLock_Caps_off           111
-#define IDI_CapsLock_Caps_on            112
+#define IDI_CapsLock                    123
 #define IDI_Caps_off                    1001
 #define IDI_Caps_on                     1002
 
@@ -11,7 +10,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        124
+#define _APS_NEXT_RESOURCE_VALUE        127
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
